f494ab5203ee0b8a690e055ded21fcfaf26fdbb7
Asia <dobrowolska.krakow@gmail.com>
2023-10-21 17:51:44 +0200

    made whole modal pop-up with logics and styles plus helpers to open modal and work with localStorage

--- src/js/helper.js
+++ src/js/helper.js
@@ -3,20 +3,18 @@ import { fetchBooksByCategory } from './api';
 
 console.log(fetchBooksByCategory('Hardcover Nonfiction'));
 
+const listBooksByCategory = document.querySelector('.list-books-by-category');
+
 const createListBooksByCategory = async selectedCategory => {
   const books = await fetchBooksByCategory(selectedCategory);
   const list = books
     .map(
-      book => `<li class="book-item" data-id="${book._id}">
-        <img class="book-item__img" src="${book.book_image}" alt="${book.title}" />
-        <h2 class="book-item__title">${book.title}</h2>
-        <p class="book-item__author">${book.author}</p>
-        <p class="book-item__publisher">${book.publisher}</p>
-        <p class="book-item__description">${book.description}</p>
-        <a class="book-item__link" href="${book.amazon_product_url}" target="_blank">Buy on Amazon</a>
-        <button class="book-item__btn-add">Add to shopping list</button>
+      book => `<li data-id="${book._id}">
+        <h4>${book.title}</h4>
+        <p>${book.author}</p>
     </li>`,
     )
     .join('');
-  return list;
+  listBooksByCategory.insertAdjacentHTML('afterbegin', list);
 };
+createListBooksByCategory('Hardcover Nonfiction');
--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,12 +1,11 @@
 'use strict';
-import { fetchBooksByCategory, fetchBookById } from './api';
+import { fetchBookById } from './api';
 
-console.log(fetchBooksByCategory('Hardcover Nonfiction'));
-console.log(fetchBookById('643282b2e85766588626a122'));
+let currentBookData = null;
 
 const bookCover = document.querySelector('.popup__img');
 const bookAmazonLink = document.querySelector('.popup__amazon');
-const bookDescription = document.querySelector('.popup__description');
+const bookDescription = document.querySelector('.popup__description-details');
 const btnAddToShoppingList = document.querySelector('.popup__btn-add');
 const btnRemoveFromShoppingList = document.querySelector('.popup__btn-remove');
 const btnClosePopUp = document.querySelector('.popup__btn-close');
@@ -29,14 +28,47 @@ const toggleButtonsVisibility = () => {
   }
 };
 
+const addToLocalStorage = () => {
+  if (!currentBookData) return;
+
+  const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
+  const bookExists = storedBooks.some(book => book._id === currentBookData._id);
+
+  if (!bookExists) {
+    storedBooks.push(currentBookData);
+    localStorage.setItem('books', JSON.stringify(storedBooks));
+  }
+
+  isBookAlreadyInShoppingList = true;
+  toggleButtonsVisibility();
+};
+
+const removeFromLocalStorage = () => {
+  if (!currentBookData) return;
+
+  const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
+  const updatedBooks = storedBooks.filter(book => book._id !== currentBookData._id);
+
+  localStorage.setItem('books', JSON.stringify(updatedBooks));
+
+  isBookAlreadyInShoppingList = false;
+  toggleButtonsVisibility();
+};
+
 const createPopUp = async bookId => {
   const bookData = await fetchBookById(bookId);
+  currentBookData = bookData;
+
+  const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
+  isBookAlreadyInShoppingList = storedBooks.some(book => book._id === bookData._id);
+  toggleButtonsVisibility();
+
   bookCover.src = bookData.book_image;
   bookAmazonLink.href = bookData.amazon_product_url;
   const markup = `<h2 class="popup__title">${bookData.title}</h2>
                 <p class="popup__author">${bookData.author}</p>
                 <p class="popup__text">${bookData.description}</p>`;
-  bookDescription.insertAdjacentHTML('afterbegin', markup);
+  bookDescription.innerHTML = markup;
 
   const shopsBooks = bookData.buy_links
     .slice(1)
@@ -45,35 +77,8 @@ const createPopUp = async bookId => {
         `<li class="popup__shops-item"><a class="popup__shops-link" href="${link.url}" target="_blank">${link.name}</a></li>`,
     )
     .join('');
-  shopsList.insertAdjacentHTML('afterbegin', shopsBooks);
-
-  const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
-  isBookAlreadyInShoppingList = storedBooks.some(book => book._id === bookData._id);
-  toggleButtonsVisibility();
-
-  const addToLocalStorage = () => {
-    if (!isBookAlreadyInShoppingList) {
-      storedBooks.push(bookData);
-      localStorage.setItem('books', JSON.stringify(storedBooks));
-      isBookAlreadyInShoppingList = true;
-      toggleButtonsVisibility();
-    }
-  };
-
-  const removeFromLocalStorage = () => {
-    if (isBookAlreadyInShoppingList) {
-      const filteredBooks = storedBooks.filter(book => book._id !== bookData._id);
-      localStorage.setItem('books', JSON.stringify(filteredBooks));
-      isBookAlreadyInShoppingList = false;
-      toggleButtonsVisibility();
-    }
-  };
-
-  btnRemoveFromShoppingList.addEventListener('click', removeFromLocalStorage);
-  btnAddToShoppingList.addEventListener('click', addToLocalStorage);
+  shopsList.innerHTML = shopsBooks;
 };
-
-createPopUp('643282b3e85766588626a1d6');
 
 const closePopUp = () => {
   backdrop.classList.add('popup-is-hidden');
@@ -97,8 +102,24 @@ const keydownHandler = e => {
   }
 };
 
-btnClosePopUp.addEventListener('click', closePopUp);
-backdrop.addEventListener('click', backdropClickHandler);
-document.addEventListener('keydown', keydownHandler);
+const listBooks = document.querySelector('.open-list');
 
+const openPopUp = e => {
+  if (e.target.closest('li')) {
+    const bookId = e.target.closest('li').dataset.id;
+    createPopUp(bookId);
+    btnClosePopUp.addEventListener('click', closePopUp);
+    backdrop.addEventListener('click', backdropClickHandler);
+    document.addEventListener('keydown', keydownHandler);
+
+    backdrop.classList.remove('popup-is-hidden');
+  }
+};
+
+listBooks.addEventListener('click', openPopUp);
+
+btnRemoveFromShoppingList.addEventListener('click', removeFromLocalStorage);
+btnAddToShoppingList.addEventListener('click', addToLocalStorage);
+
+// openPopUp();
 // localStorage.clear();
