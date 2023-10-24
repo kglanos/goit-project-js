30fbaf3a0ab58e3ef9b07b6d817ffad4cb17aa83
Mart <marta.nikiel@yahoo.com>
2023-10-24 14:42:25 +0200

    added validation in js; changed  scss

--- src/js/modal-registration.js
+++ src/js/modal-registration.js
@@ -10,3 +10,60 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+const form = document.querySelector('.registration-modal-form');
+const btnSubmit = document.querySelector('.signup-btn');
+const inputName = document.querySelector('#user-name');
+const inputEmail = document.querySelector('#user-email');
+
+form.reset();
+
+const regexName = /^[\p{L}\s]+$/u;
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regexTextarea = /^[\p{L}\p{N}\p{P}\p{S}\p{Z}\s]{5,1000}$/u;
+
+const validateButton = (button, elements) => {
+  const allValid = elements.every(element => element.classList.contains('valid'));
+  if (allValid) {
+    button.disabled = false;
+    button.classList.remove('disabled');
+  } else {
+    button.disabled = true;
+    button.classList.add('disabled');
+  }
+};
+
+const doValidation = (element, message, regex, button, elements) => {
+  if (!regex.test(element.value)) {
+    element.classList.remove('valid');
+    element.classList.add('invalid');
+    message.classList.remove('warning-valid');
+    message.classList.add('warning-invalid');
+  } else {
+    element.classList.add('valid');
+    element.classList.remove('invalid');
+    message.classList.add('warning-valid');
+    message.classList.remove('warning-invalid');
+  }
+  validateButton(button, elements);
+};
+
+const allElementsToValidate = [inputName, inputEmail];
+
+inputName.addEventListener('input', () =>
+  doValidation(inputName, warningMessageName, regexName, btnSubmit, allElementsToValidate),
+);
+inputName.addEventListener('blur', () =>
+  doValidation(inputName, warningMessageName, regexName, btnSubmit, allElementsToValidate),
+);
+inputEmail.addEventListener('input', () =>
+  doValidation(inputEmail, warningMessageEmail, regexEmail, btnSubmit, allElementsToValidate),
+);
+inputEmail.addEventListener('blur', () =>
+  doValidation(inputEmail, warningMessageEmail, regexEmail, btnSubmit, allElementsToValidate),
+);
+
+// eslint-disable-next-line no-unused-vars
+document.addEventListener('DOMContentLoaded', event => {
+  form.reset();
+});
