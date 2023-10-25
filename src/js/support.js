a5e596717b553b5b84828561d6bf49ea4e18faa9
zanetamak <zaneta.zawislak@gmail.com>
2023-10-25 19:44:27 +0200

    added charities icons

--- src/js/support.js
+++ src/js/support.js
@@ -1,17 +1,38 @@
 import charities from './charities';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
-function createList() {
+import 'swiper/swiper.min.css';
 
-    const ol = document.createElement('ol');
+const list = document.querySelector('.support__list');
 
-    charities.forEach((charitie, index) => {
-        const li = document.createElement('li');
-        li.textContent = `${index + 1}. ${charitie.title}`;
-        ol.appendChild(li);
-    });
+const html = charities.map(makeMarkup).join('');
 
+function makeMarkup({ url, title, img }, index) {
+  const digits = (index + 1).toString().padStart(2, '0');
 
-    document.getElementById('support__list').appendChild(ol);
+  return `
+  <li class="swiper-slide">
+        <div class="support__item">
+        <span class="support__index">${digits}</span>
+        <a class="support__link" href="${url}" target="_blank" rel="noopener noreferrer nofollow">
+            <img
+                srcset="${img}" 1x
+                src="${img}" type="image/png" alt="${title}">
+        </a>
+    </li>`;
 }
 
-createList();
+list.innerHTML = html;
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  slidesPerView: 6,
+  rewind: true,
+
+  effect: 'slide',
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-next',
+  },
+});
