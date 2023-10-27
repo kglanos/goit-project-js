51656e95edf31e8e7ebbd1b95d688e175f6a5d33
kglanos <130902847+kglanos@users.noreply.github.com>
2023-10-27 22:41:13 +0200

    zmiany

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,13 +1,9 @@
 import './sass/main.scss';
 import './js/mobile-menu';
 import './js/theme-changer';
-import './js/scroll-up';
 import './js/navigation-highlight';
-import './js/book-categories';
 import './js/support.js';
-import './js/books-gallery';
-import './js/booklist';
 import './js/modal-registration';
-import './js/pop-up';
+import './js/shopping-list';
 import './js/firebase';
-
+import './js/scroll-up';
