5982341c5788963d5e77dab92736858ced4b1ee1
Dawid <dawidpiecha88@gmail.com>
2023-10-30 23:08:41 +0100

    fixed theme-changing js and it's connection to pop-up.scss

--- src/js/theme-changer.js
+++ src/js/theme-changer.js
@@ -3,34 +3,26 @@ const body = document.querySelector('body');
 
 let theme = localStorage.getItem('theme') || 'light-theme';
 
+function setThemeButtonIconPosition() {
+  const themeButtonIcon = themeButton.querySelector('.theme-button__icon');
+  if (theme === 'dark-theme') {
+    themeButtonIcon.style.transform = 'translateX(21px)';
+  } else {
+    themeButtonIcon.style.transform = 'translateX(0)';
+  }
+}
+setThemeButtonIconPosition();
+
 body.classList.add(theme);
 
 themeButton.addEventListener('click', () => {
-  const btnAddToShoppingList = document.querySelector('.popup__btn-add');
-  const btnRemoveFromShoppingList = document.querySelector('.popup__btn-remove');
-  const popUp = document.querySelector('.popup');
-  const comment = document.querySelector('.popup__comment');
-  const iconClose = document.querySelector('.popup__icon-close');
-  const amazonLogo = document.querySelector('.popup__amazon');
-
   if (theme === 'dark-theme') {
     body.classList.remove('dark-theme');
-    popUp.classList.remove('popup-dark-theme');
-    iconClose.classList.remove('icon-close-dark-theme');
-    btnAddToShoppingList.classList.remove('btn-add-dark-theme');
-    btnRemoveFromShoppingList.classList.remove('btn-remove-dark-theme');
-    comment.classList.remove('comment-dark-theme');
-    amazonLogo.classList.remove('amazon-dark-theme');
     theme = 'light-theme';
   } else {
     body.classList.add('dark-theme');
-    popUp.classList.add('popup-dark-theme');
-    iconClose.classList.add('icon-close-dark-theme');
-    btnAddToShoppingList.classList.add('btn-add-dark-theme');
-    btnRemoveFromShoppingList.classList.add('btn-remove-dark-theme');
-    comment.classList.add('comment-dark-theme');
-    amazonLogo.classList.add('amazon-dark-theme');
     theme = 'dark-theme';
   }
+  setThemeButtonIconPosition();
   localStorage.setItem('theme', theme);
 });
