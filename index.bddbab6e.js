4f04f5e475200534759653e9400020d050563dd3
kglanos <130902847+kglanos@users.noreply.github.com>
2023-11-01 20:12:25 +0000

    Deploying to gh-pages from @ kglanos/goit-project-js@ac41768f49ec5abaab015af7ef111468e73095c2 🚀

--- index.bddbab6e.js
+++ index.bddbab6e.js
@@ -1,4 +1,4 @@
-function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.parcelRequired7c6;null==a&&((a=function(e){if(e in o)return o[e].exports;if(e in r){var t=r[e];delete r[e];var a={id:e,exports:{}};return o[e]=a,t.call(a.exports,a,a.exports),a.exports}var i=Error("Cannot find module '"+e+"'");throw i.code="MODULE_NOT_FOUND",i}).register=function(e,t){r[e]=t},t.parcelRequired7c6=a),a.register,a("kM5jX"),a("haS5e");var i={},l={},n={};n=/**
+function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},i=t.parcelRequired7c6;null==i&&((i=function(e){if(e in o)return o[e].exports;if(e in r){var t=r[e];delete r[e];var i={id:e,exports:{}};return o[e]=i,t.call(i.exports,i,i.exports),i.exports}var a=Error("Cannot find module '"+e+"'");throw a.code="MODULE_NOT_FOUND",a}).register=function(e,t){r[e]=t},t.parcelRequired7c6=i),i.register,i("kM5jX"),i("haS5e");var a={},l={},n={};n=/**
  * Checks if `value` is the
  * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
  * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
@@ -22,14 +22,14 @@ function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.
  *
  * _.isObject(null);
  * // => false
- */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var s={},c={},d={};d="object"==typeof t&&t&&t.Object===Object&&t;/** Detect free variable `self`. */var u="object"==typeof self&&self&&self.Object===Object&&self;c=d||u||Function("return this")(),s=function(){return c.Date.now()};var g={},b={},_={},v=/\s/;_=/**
+ */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var s={},c={},d={};d="object"==typeof t&&t&&t.Object===Object&&t;/** Detect free variable `self`. */var u="object"==typeof self&&self&&self.Object===Object&&self;c=d||u||Function("return this")(),s=function(){return c.Date.now()};var b={},g={},_={},v=/\s/;_=/**
  * Used by `_.trim` and `_.trimEnd` to get the index of the last non-whitespace
  * character of `string`.
  *
  * @private
  * @param {string} string The string to inspect.
  * @returns {number} Returns the index of the last non-whitespace character.
- */function(e){for(var t=e.length;t--&&v.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var y=/^\s+/;b=/**
+ */function(e){for(var t=e.length;t--&&v.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var y=/^\s+/;g=/**
  * The base implementation of `_.trim`.
  *
  * @private
@@ -41,7 +41,7 @@ function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the raw `toStringTag`.
- */function(e){var t=$.call(e,j),o=e[j];try{e[j]=void 0;var r=!0}catch(e){}var a=L.call(e);return r&&(t?e[j]=o:delete e[j]),a};var T={},E=Object.prototype.toString;T=/**
+ */function(e){var t=$.call(e,j),o=e[j];try{e[j]=void 0;var r=!0}catch(e){}var i=L.call(e);return r&&(t?e[j]=o:delete e[j]),i};var T={},E=Object.prototype.toString;T=/**
  * Converts `value` to a string using `Object.prototype.toString`.
  *
  * @private
@@ -92,7 +92,7 @@ function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.
  *
  * _.isSymbol('abc');
  * // => false
- */function(e){return"symbol"==typeof e||w(e)&&"[object Symbol]"==k(e)};/** Used as references for various `Number` constants. */var O=0/0,x=/^[-+]0x[0-9a-f]+$/i,M=/^0b[01]+$/i,q=/^0o[0-7]+$/i,C=parseInt;g=/**
+ */function(e){return"symbol"==typeof e||w(e)&&"[object Symbol]"==k(e)};/** Used as references for various `Number` constants. */var O=0/0,x=/^[-+]0x[0-9a-f]+$/i,M=/^0b[01]+$/i,q=/^0o[0-7]+$/i,H=parseInt;b=/**
  * Converts `value` to a number.
  *
  * @static
@@ -114,7 +114,7 @@ function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.
  *
  * _.toNumber('3.2');
  * // => 3.2
- */function(e){if("number"==typeof e)return e;if(f(e))return O;if(n(e)){var t="function"==typeof e.valueOf?e.valueOf():e;e=n(t)?t+"":t}if("string"!=typeof e)return 0===e?e:+e;e=b(e);var o=M.test(e);return o||q.test(e)?C(e.slice(2),o?2:8):x.test(e)?O:+e};/* Built-in method references for those with the same name as other `lodash` methods. */var H=Math.max,N=Math.min;l=/**
+ */function(e){if("number"==typeof e)return e;if(f(e))return O;if(n(e)){var t="function"==typeof e.valueOf?e.valueOf():e;e=n(t)?t+"":t}if("string"!=typeof e)return 0===e?e:+e;e=g(e);var o=M.test(e);return o||q.test(e)?H(e.slice(2),o?2:8):x.test(e)?O:+e};/* Built-in method references for those with the same name as other `lodash` methods. */var C=Math.max,N=Math.min;l=/**
  * Creates a debounced function that delays invoking `func` until after `wait`
  * milliseconds have elapsed since the last time the debounced function was
  * invoked. The debounced function comes with a `cancel` method to cancel
@@ -167,16 +167,16 @@ function e(e){return e&&e.__esModule?e.default:e}var t=globalThis,o={},r={},a=t.
  *
  * // Cancel the trailing debounced invocation.
  * jQuery(window).on('popstate', debounced.cancel);
- */function(e,t,o){var r,a,i,l,c,d,u=0,b=!1,_=!1,v=!0;if("function"!=typeof e)throw TypeError("Expected a function");function y(t){var o=r,i=a;return r=a=void 0,u=t,l=e.apply(i,o)}function f(e){var o=e-d,r=e-u;// Either this is the first call, activity has stopped and we're at the
+ */function(e,t,o){var r,i,a,l,c,d,u=0,g=!1,_=!1,v=!0;if("function"!=typeof e)throw TypeError("Expected a function");function y(t){var o=r,a=i;return r=i=void 0,u=t,l=e.apply(a,o)}function f(e){var o=e-d,r=e-u;// Either this is the first call, activity has stopped and we're at the
 // trailing edge, the system time has gone backwards and we're treating
 // it as the trailing edge, or we've hit the `maxWait` limit.
-return void 0===d||o>=t||o<0||_&&r>=i}function k(){var e,o,r,a=s();if(f(a))return h(a);// Restart the timer.
-c=setTimeout(k,(e=a-d,o=a-u,r=t-e,_?N(r,i-o):r))}function h(e){return(// Only invoke if we have `lastArgs` which means `func` has been
+return void 0===d||o>=t||o<0||_&&r>=a}function k(){var e,o,r,i=s();if(f(i))return h(i);// Restart the timer.
+c=setTimeout(k,(e=i-d,o=i-u,r=t-e,_?N(r,a-o):r))}function h(e){return(// Only invoke if we have `lastArgs` which means `func` has been
 // debounced at least once.
-(c=void 0,v&&r)?y(e):(r=a=void 0,l))}function m(){var e,o=s(),i=f(o);if(r=arguments,a=this,d=o,i){if(void 0===c)return(// Reset any `maxWait` timer.
+(c=void 0,v&&r)?y(e):(r=i=void 0,l))}function m(){var e,o=s(),a=f(o);if(r=arguments,i=this,d=o,a){if(void 0===c)return(// Reset any `maxWait` timer.
 u=e=d,// Start the timer for the trailing edge.
-c=setTimeout(k,t),b?y(e):l);if(_)return(// Handle invocations in a tight loop.
-clearTimeout(c),c=setTimeout(k,t),y(d))}return void 0===c&&(c=setTimeout(k,t)),l}return t=g(t)||0,n(o)&&(b=!!o.leading,i=(_="maxWait"in o)?H(g(o.maxWait)||0,t):i,v="trailing"in o?!!o.trailing:v),m.cancel=function(){void 0!==c&&clearTimeout(c),u=0,r=d=a=c=void 0},m.flush=function(){return void 0===c?l:h(s())},m},i=/**
+c=setTimeout(k,t),g?y(e):l);if(_)return(// Handle invocations in a tight loop.
+clearTimeout(c),c=setTimeout(k,t),y(d))}return void 0===c&&(c=setTimeout(k,t)),l}return t=b(t)||0,n(o)&&(g=!!o.leading,a=(_="maxWait"in o)?C(b(o.maxWait)||0,t):a,v="trailing"in o?!!o.trailing:v),m.cancel=function(){void 0!==c&&clearTimeout(c),u=0,r=d=i=c=void 0},m.flush=function(){return void 0===c?l:h(s())},m},a=/**
  * Creates a throttled function that only invokes `func` at most once per
  * every `wait` milliseconds. The throttled function comes with a `cancel`
  * method to cancel delayed `func` invocations and a `flush` method to
@@ -219,11 +219,11 @@ clearTimeout(c),c=setTimeout(k,t),y(d))}return void 0===c&&(c=setTimeout(k,t)),l
  *
  * // Cancel the trailing throttled invocation.
  * jQuery(window).on('popstate', throttled.cancel);
- */function(e,t,o){var r=!0,a=!0;if("function"!=typeof e)throw TypeError("Expected a function");return n(o)&&(r="leading"in o?!!o.leading:r,a="trailing"in o?!!o.trailing:a),l(e,t,{leading:r,maxWait:t,trailing:a})};const U=document.querySelector(".scroll-to-top"),A=/*@__PURE__*/e(i)(function(){let e=window.scrollY,t=document.documentElement.clientHeight;function o(){window.scrollTo({top:0,behavior:"smooth"})}e>=t?(U.addEventListener("click",o),U.classList.remove("unvisually")):(U.classList.add("unvisually"),U.addEventListener("click",o))},500);window.addEventListener("scroll",A),a("fQwj1");var B=(a("lXnOV"),a("lXnOV")),D=a("7IoHk");const I=document.querySelector(".category__title"),R=document.querySelector(".category__list"),V=document.querySelector(".side-bar__categories"),X=document.querySelector(".books-gallery"),W=document.querySelector(".category"),F=async t=>{let o=await (0,B.fetchBooksByCategory)(t);if(0===o.length){/*@__PURE__*/e(D).Notify.info("Unfortunately, we do not have these books at the moment.");return}let r=o.map(e=>`<li class="category__item" data-id="${e._id}">
+ */function(e,t,o){var r=!0,i=!0;if("function"!=typeof e)throw TypeError("Expected a function");return n(o)&&(r="leading"in o?!!o.leading:r,i="trailing"in o?!!o.trailing:i),l(e,t,{leading:r,maxWait:t,trailing:i})};const U=document.querySelector(".scroll-to-top"),A=/*@__PURE__*/e(a)(function(){let e=window.scrollY,t=document.documentElement.clientHeight;function o(){window.scrollTo({top:0,behavior:"smooth"})}e>=t?(U.addEventListener("click",o),U.classList.remove("unvisually")):(U.classList.add("unvisually"),U.addEventListener("click",o))},500);window.addEventListener("scroll",A),i("fQwj1");var B=(i("lXnOV"),i("lXnOV")),D=i("7IoHk");const I=document.querySelector(".category__title"),R=document.querySelector(".category__list"),V=document.querySelector(".side-bar__categories"),X=document.querySelector(".books-gallery"),W=document.querySelector(".category"),F=async t=>{let o=await (0,B.fetchBooksByCategory)(t);if(0===o.length){/*@__PURE__*/e(D).Notify.info("Unfortunately, we do not have these books at the moment.");return}let r=o.map(e=>`<li class="category__item" data-id="${e._id}">
         <div class="category__cover"><img class="category__image" alt="book cover" src="${e.book_image}"/></div>
         <div class="category__image-description"><h4 class="category__book-title">${e.title}</h4>
         <p class="category__book-author">${e.author}</p></div> 
-    </li>`).join("");R.innerHTML=r},P=async e=>{if(e.target.closest("li")&&"All categories"!=e.target.textContent){X.classList.add("gallery-hidden"),W.classList.remove("gallery-hidden");let t=e.target.closest("li").textContent,o=t.split(" "),r=o.pop(),a=o.join(" ");I.innerHTML=`${a} <span class="last-word-color">${r}</span>`,R.innerHTML="",F(t)}else if("SEE MORE"===e.target.textContent){X.classList.add("gallery-hidden"),W.classList.remove("gallery-hidden"),selectedCategory=e.target.name;let t=selectedCategory.split(" "),o=t.pop(),r=t.join(" ");I.innerHTML=`${r} <span class="last-word-color">${o}</span>`,R.innerHTML="",F(selectedCategory)}};V.addEventListener("click",P);var D=a("7IoHk");const Q=document.querySelector(".books-gallery"),Y=document.querySelector(".side-bar__categories-item"),z=document.querySelector(".category");!async function(){let t=await (0,B.fetchTopBooksInCategories)(),o=t.slice(0,4);if(0===o.length){/*@__PURE__*/e(D).Notify.info("Unfortunately, we do not have these books at the moment.");return}let r=o.map(e=>`<div>
+    </li>`).join("");R.innerHTML=r},P=async e=>{if(e.target.closest("li")&&"All categories"!=e.target.textContent){X.classList.add("gallery-hidden"),W.classList.remove("gallery-hidden");let t=e.target.closest("li").textContent,o=t.split(" "),r=o.pop(),i=o.join(" ");I.innerHTML=`${i} <span class="last-word-color">${r}</span>`,R.innerHTML="",F(t)}else if("SEE MORE"===e.target.textContent){X.classList.add("gallery-hidden"),W.classList.remove("gallery-hidden");let t=e.target.name,o=t.split(" "),r=o.pop(),i=o.join(" ");I.innerHTML=`${i} <span class="last-word-color">${r}</span>`,R.innerHTML="",F(t)}};V.addEventListener("click",P);var D=i("7IoHk");const Q=document.querySelector(".books-gallery"),Y=document.querySelector(".side-bar__categories-item"),z=document.querySelector(".category");!async function(){let t=await (0,B.fetchTopBooksInCategories)(),o=t.slice(0,4);if(0===o.length){/*@__PURE__*/e(D).Notify.info("Unfortunately, we do not have these books at the moment.");return}let r=o.map(e=>`<div>
         <h2 class="books-gallery__category-title">${e.list_name}</h2>
     <ul class="books-gallery__section">
 
@@ -240,6 +240,6 @@ clearTimeout(c),c=setTimeout(k,t),y(d))}return void 0===c&&(c=setTimeout(k,t)),l
     </ul>
     
     <div class="books-gallery__button-wrapper"><button name="${e.list_name}" type="button" class="books-gallery__button">SEE MORE</button></div>
-    </div>`).join("");Q.insertAdjacentHTML("beforeend",r),function(){let e=document.querySelectorAll(".books-gallery__button");e.forEach(e=>e.addEventListener("click",P))}()}(),Y.addEventListener("click",()=>{Q.classList.remove("gallery-hidden"),z.classList.add("gallery-hidden")});var B=a("lXnOV");const G=document.querySelector(".side-bar__categories");(async function(){let e=await (0,B.fetchBooksCategoryList)(),t=e.map(e=>`<li tabindex='-1' class="side-bar__categories-item">${e.list_name}</li>`).join("");G.insertAdjacentHTML("beforeend",t)})(),a("a7nVE"),a("1fSMk"),a("2DTLi"),a("eNCUi");//# sourceMappingURL=index.d4abe945.js.map
+    </div>`).join("");Q.insertAdjacentHTML("beforeend",r),function(){let e=document.querySelectorAll(".books-gallery__button");e.forEach(e=>e.addEventListener("click",P))}()}(),Y.addEventListener("click",()=>{Q.classList.remove("gallery-hidden"),z.classList.add("gallery-hidden")});var B=i("lXnOV");const G=document.querySelector(".side-bar__categories");(async function(){let e=await (0,B.fetchBooksCategoryList)(),t=e.map(e=>`<li tabindex='-1' class="side-bar__categories-item">${e.list_name}</li>`).join("");G.insertAdjacentHTML("beforeend",t)})(),i("a7nVE"),i("1fSMk"),i("2DTLi"),i("eNCUi");//# sourceMappingURL=index.bddbab6e.js.map
 
-//# sourceMappingURL=index.d4abe945.js.map
+//# sourceMappingURL=index.bddbab6e.js.map
